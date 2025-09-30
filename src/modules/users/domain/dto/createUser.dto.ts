9ab30677aa2aa406df412a16c081f8b1c4787e1c
Juan Useche <juanuseche47@gmail.com>
2025-09-30 01:35:01 -0300

    feat: abstract is id exists and create user dto

---
+++ src/modules/users/domain/dto/createUser.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+import { Role } from "generated/prisma";
+
+
+
+
+
+
+
+export class CreateUserDTO {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsEnum(Role)
+    role: Role;
+}
